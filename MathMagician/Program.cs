cadf379b9e043d997a9386b2156cbfa824789f3d
jessica kilby <jessicakilby@gmail.com>
2017-01-24 20:39:32 -0600

    Instructor assisted Step 0. We should complete the rest.

--- MathMagician/Program.cs
+++ MathMagician/Program.cs
@@ -10,6 +10,28 @@ namespace MathMagician
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What would you like for me to do?");
+            Console.WriteLine(""); //How you provide the user useful info is on you.
+            //think about, 'How will the user pick a command?'
+            //Is it better to modify the original question? Make it more useful?
+
+            string command = Console.ReadLine();
+            //Once I have the command (whatever it looks like), how do I chekc if 
+            //it's a valid command?
+
+            //When should you actually create an instance of your number class? now or later?
+            Console.WriteLine("How many should I print?");
+
+            string howMany = Console.ReadLine();
+
+            //When do I check if the "HowMany" response is too big? no more than 30
+            //If they inter a number > 30, how do I respond and exit the program?
+
+            Console.WriteLine($"Cool, I'm going to print {howMany} {command} numbers.");
+
+            //okay!! Print some stuff!
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
