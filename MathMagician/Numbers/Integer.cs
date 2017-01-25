96f4c5e457e8d1bf0c48af4813fb6b0f854a18f8
abbie <abigail.c.holder@gmail.com>
2017-01-24 20:42:59 -0600

    initial in-class setup

--- MathMagician/Numbers/Integer.cs
+++ MathMagician/Numbers/Integer.cs
@@ -10,7 +10,7 @@ namespace MathMagician.Numbers
     {
         int GetFirst(); //first number in sequence of numbers that's printed to screen
         int GetNext(int current); // given a current number, find way to calculate the next number
-        int[] GetSequence(int how_many); // get how many numbers you want printed out and prints an array
-        string printNumbers(int[] how_many); // print the above
+        int[] GetSequence(int howMany); // get how many numbers you want printed out and prints an array
+        string printNumbers(int[] howMany); // print the above
     }
 }
