53d556f2a5845b4d8340cb86ce9ef5a76be5a4e7
Taylor Harry <taylor.andrew.harry@gmail.com>
2017-01-24 18:39:16 -0800

    instructor assisted setup

--- MathMagician/Program.cs
+++ MathMagician/Program.cs
@@ -10,6 +10,21 @@ namespace MathMagician
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What would you like for me to retrieve?");
+            Console.WriteLine("  1. Natural numbers" + Environment.NewLine + "  2. Even Numbers" + Environment.NewLine + "  3. Odd Numbers" + Environment.NewLine + "  4. Fibonacci Numbers" + Environment.NewLine + "  5. Prime Numbers"); //pick a number to start from menu options
+            //has a valid command been entered?
+            string command = Console.ReadLine();
+
+            Console.WriteLine("How many should I print?");
+
+            string howMany = Console.ReadLine();
+            //has it been entered as less than 30?
+
+            Console.WriteLine($"Cool, I am going to print {howMany} {command} numbers.");
+
+            //OK print some stuff!
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
