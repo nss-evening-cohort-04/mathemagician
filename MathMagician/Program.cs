87a5ebb7357b1a3508ec07cd8e3ee2fcb262caa1
Thomas Waters <Thomas.waters05@gmail.com>
2017-01-24 18:39:33 -0800

    Instructor assisted Step 0. I will complete the rest

--- MathMagician/Program.cs
+++ MathMagician/Program.cs
@@ -10,6 +10,31 @@ namespace MathMagician
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What would you like for me to do?");
+            Console.WriteLine(""); //"How" you provide the user useful info is your choice TomBO (you could have them enter a number or enter specific text)
+                                   //Think about, 'How will the user pick a command?'
+                                   // Is it better to modify the original question? Make it more useful?
+
+            Console.WriteLine("1.Add this later" + Environment.NewLine);
+
+            string commandz = Console.ReadLine(); //this is "reading" what the user types in
+            // saying string commandz is saving 
+            //the type 'string' as commandz
+            //but how do we know this a valid command?
+
+            Console.WriteLine("How many should I print?");
+
+            string how_many = Console.ReadLine();//this is "reading" what the user types in
+
+            //When do I check if the "How many" response is too big?
+            //If they enter a number > 30, how do I respond AND exit the program?
+
+            Console.WriteLine($"Cool, I'm going to print {how_many} {commandz} numbers.");
+
+            //Okay!!! Print some stuff!!
+
+            Console.WriteLine("Press any key to exit..");
+            Console.ReadKey();
         }
     }
 }
