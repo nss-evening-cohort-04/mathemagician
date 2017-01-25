f2eeaa866c239456687230b595b1728832cf3429
Andi DeVois <adevois@gmail.com>
2017-01-24 21:32:08 -0600

    Refactored - added a constructor setting First private property  to generalize GetFirst behavior

---
+++ MathMagician/Numbers/NaturalNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMagician.Numbers
+{
+    public class NaturalNumber : Integer
+    {
+        private int First { get; set; }
+
+        public NaturalNumber()
+        {
+            First = 1;
+            //this.First = 1;
+        }
+        
+        public int GetFirst()
+        {
+            return First;
+        }
+
+        public int GetNext(int current)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int[] GetSequence(int how_many)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string PrintNumbers(int[] how_many)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MathMagicianTests/Numbers/NaturalNumberTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathMagician.Numbers;
+
+namespace MathMagicianTests.Numbers
+{
+    [TestClass]
+    public class NaturalNumberTests
+    {
+        //This is a "sanity check" test.
+        //Ensures everything is hooked up correctly AND that the permissions are correct.
+        [TestMethod]
+        public void EnsureICanCreateInstance()
+        {
+            NaturalNumber naturalNumber = new NaturalNumber();
+
+            Assert.IsNotNull(naturalNumber);
+        }
+
+        [TestMethod]
+        public void EnsureOneIsTheFirstNumber()
+        {
+            //Arrange (set up)
+            NaturalNumber naturalNumber = new NaturalNumber();
+
+            //Act (call method being tested)
+            int exepectedResult = 1;
+            int actualResult = naturalNumber.GetFirst();
+
+            //Assert (check output from method)
+            Assert.AreEqual(exepectedResult, actualResult);
+
+        }
+    }
+}
