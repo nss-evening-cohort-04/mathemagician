be0bc99a3b6bbdd1bd437b9182325859bbdb5479
Taylor Harry <taylor.andrew.harry@gmail.com>
2017-01-24 19:31:32 -0800

    globalized GetFirst method behavior

--- MathMagician/Numbers/NaturalNumber.cs
+++ MathMagician/Numbers/NaturalNumber.cs
@@ -8,11 +8,18 @@ namespace MathMagician.Numbers
 {
     public class NaturalNumber : Integer
     {
+        //don't want them to access it outside of the class//
+        private int FirstNumber { get; set; }
 
+        public NaturalNumber()
+        {
+            FirstNumber = 1;
+            //this.FirstNumber = 1;
+        }
 
         public int GetFirst()
         {
-            return 1;
+            return FirstNumber;
         }
 
         public int GetNext(int current)
