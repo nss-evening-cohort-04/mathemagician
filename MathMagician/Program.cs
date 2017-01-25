0c20e41bf0c530ca432e583abbb3756ee07bd6a6
Debra Gordon <debra.m.gordon@gmail.com>
2017-01-24 18:41:00 -0800

    started assignment with the teacher

--- MathMagician/Program.cs
+++ MathMagician/Program.cs
@@ -10,6 +10,24 @@ namespace MathMagician
     {
         static void Main(string[] args)
         {
+            // Ask the user how they feel
+            Console.WriteLine("In five words or less, tell me how you are feeling today ...");
+            // capture their response in a variable
+            string feeling = Console.ReadLine();
+
+            // Ask the user how many hours they slept last night
+            Console.WriteLine("How many hours did you sleep last night?");
+            // capture their response in a variable
+            string timeSlept = Console.ReadLine();
+
+            // write the result to the console
+            Console.WriteLine($"You got { timeSlept } hours of sleep last night and that makes you feel like { feeling }.");
+
+            // prgram will quit when ...
+            Console.WriteLine("Press any key to quit and get some more rest to face another day");
+            Console.ReadKey();
+
+
         }
     }
 }
