85967b2e5ff2affc1cfa8e926dec606f51943b02
Debra Gordon <debra.m.gordon@gmail.com>
2017-01-24 19:17:05 -0800

    working through unit tests

---
+++ MathMagician/Numbers/NaturalNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMagician.Numbers
+{
+    public class NaturalNumber : Integer
+    {
+        public int GetFirst()
+        {
+            return 1;
+        }
+
+        public int GetNext(int current)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int[] GetSequence(int howMany)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string printNumbers(int[] howMany)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
