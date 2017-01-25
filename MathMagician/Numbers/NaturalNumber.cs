6bd0526d01f2287fb0fbc0a97a5376fbcc6bf4cc
sward42 <sarahward42@gmail.com>
2017-01-24 21:19:20 -0600

    implement and test GetFirst

---
+++ MathMagician/Numbers/NaturalNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMagician.Numbers
+{
+    public class NaturalNumber : Integer
+    {
+
+
+        public int GetFirst()
+        {
+            return 1;
+        }
+
+        public int GetNext(int current)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int[] GetSequence(int how_many)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string printNumbers(int[] how_many)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
