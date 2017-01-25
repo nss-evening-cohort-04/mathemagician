25a5bbdcb55c10dbcb74a5c2d340197a6184e6e9
jessica kilby <jessicakilby@gmail.com>
2017-01-24 21:18:31 -0600

    Fised casing on interface method

---
+++ MathMagicianTests/Numbers/NaturalNumberTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathMagician.Numbers;
+
+namespace MathMagicianTests.Numbers
+{
+    [TestClass]
+    public class NaturalNumberTests
+    {
+        //This a sanity check test.
+        //Ensures everything is hooked up correctly and
+        //that the permissions are correct.
+        [TestMethod]
+        public void EnsureICanCreateInstance()
+        {
+            NaturalNumber naturalNumber = new NaturalNumber();
+
+            Assert.IsNotNull(naturalNumber);
+        }
+
+        [TestMethod]
+        public void EnsureOneIsTheFirstNumber()
+        {
+            //Arrange (Where you set stuff up)
+            NaturalNumber naturalNumber = new NaturalNumber();
+
+            //Act (Call the method you're testing) test one method at the time.
+            int expectedResult = 1;
+            int actualResult = naturalNumber.GetFirst();
+
+            //Assert (Check the output from your method.)
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+    }
+}
