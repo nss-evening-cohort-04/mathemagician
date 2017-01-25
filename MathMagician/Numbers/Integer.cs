392f8ebf27ec318861cccdc5284f2178bd0f3e18
abbie <abigail.c.holder@gmail.com>
2017-01-24 20:12:39 -0600

    GetSequence should return an array

--- MathMagician/Numbers/Integer.cs
+++ MathMagician/Numbers/Integer.cs
@@ -8,9 +8,9 @@ namespace MathMagician.Numbers
 {
     interface Integer
     {
-        int GetFirst();
-        int GetNext(int current);
-        int GetSequence(int how_many);
-        string printNumbers(int[] how_many);
+        int GetFirst(); //first number in sequence of numbers that's printed to screen
+        int GetNext(int current); // given a current number, find way to calculate the next number
+        int[] GetSequence(int how_many); // get how many numbers you want printed out and prints an array
+        string printNumbers(int[] how_many); // print the above
     }
 }
