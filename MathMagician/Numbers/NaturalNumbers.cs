1490a17e47bcd8ac30a5e877e1cb8f70f0d819da
Josh Kilgore <kilgore.josh@gmail.com>
2017-01-24 21:32:45 -0600

    add tests and first class and methods

---
+++ MathMagician/Numbers/NaturalNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMagician.Numbers
+{
+    public class NaturalNumbers : Integer
+    {
+        private int First { get; set; }
+
+        public NaturalNumbers()
+        {
+            First = 1;
+            //this.First = 1;
+        }
+
+        public int GetFirst()
+        {
+           return 1;
+        }
+
+        public int GetNext(int current)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int[] GetSequence(int how_many)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string printNumbers(int[] how_many)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MathMagicianTests/Numbers/NaturalNumberTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathMagician.Numbers;
+
+namespace MathMagicianTests.Numbers
+{
+    [TestClass]
+    public class NaturalNumberTest
+    {
+        [TestMethod]
+        public void EnsureICanCreatInstance()
+        {
+            NaturalNumbers naturalNumber = new NaturalNumbers();
+
+            Assert.IsNotNull(naturalNumber);
+        }
+
+        [TestMethod]
+        public void EnsureOneIsTheFirstNumber()
+        {
+            //Arrange (set stuff up)
+            NaturalNumbers naturalNumber = new NaturalNumbers();
+
+            //Act (call method you are testing)
+            int expectedResult = 1;
+            int actualResult = naturalNumber.GetFirst();
+
+
+            //Assert (check output)
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+    }
+}
