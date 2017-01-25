f27d4cba1270ed103254bfacb20f808cea9e01ca
Josh Kilgore <kilgore.josh@gmail.com>
2017-01-24 20:43:49 -0600

    add first class

--- MathMagician/Program.cs
+++ MathMagician/Program.cs
@@ -10,6 +10,19 @@ namespace MathMagician
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What would you like for me to do?");
+
+            string command = Console.ReadLine();
+
+            Console.WriteLine("How many should I print?");
+
+            string how_many = Console.ReadLine();
+
+            Console.WriteLine($"Cool, I'm going to print {how_many} {command} numbers.");
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
         }
     }
 }
