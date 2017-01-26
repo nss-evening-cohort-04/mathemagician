69f4ebf28cc532c0a62ba7e947a6a94f742278e1
LibE4 <libin_75@hotmail.com>
2017-01-25 17:48:36 -0800

    natural number done

--- MathMagician/Program.cs
+++ MathMagician/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathMagician.Numbers;
 
 namespace MathMagician
 {
@@ -10,6 +11,43 @@ namespace MathMagician
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What would you like me to Do?");
+            Console.WriteLine("Hint: \"natural\" for natural numbers;");
+            Console.WriteLine(); // "How" you provide the user useful info is on you.
+            // Think about, 'How will teh user pick a command?'
+            // Is it better to modify the original question? Make it more useful?
+            string command = Console.ReadLine();
+            // Once I have the command (whatever it looks like), how do I check if
+            // it's a valid command?
+            if( command.ToLower() == "natural")
+            {
+                NaturalNumber naturalNumber = new NaturalNumber();
+
+                // when should you actually create an instance of your number class? now or later?
+                Console.WriteLine("How many should I print?");
+                string how_many = Console.ReadLine();
+                // when do I check if the "How Many" response it too big?
+                // If they enter a number > 30, how do I respond AND exit the program?
+                int totalNum = Int32.Parse(how_many);
+                if(totalNum <= 30 && totalNum >=1)
+                {
+                    Console.WriteLine($"Cool, I'm going to print {how_many} {command} numbers.");
+                    // Okay!!! Print some stuff!!!
+                    string actualResult = naturalNumber.PrintNumbers(naturalNumber.GetSequence(totalNum));
+                    Console.WriteLine(actualResult);
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, the number is out of range!");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Sorry, wrong command!");
+            }
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
---
+++ MathMagicianTests/Numbers/NaturalNumberTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathMagician.Numbers;
+
+namespace MathMagicianTests.Numbers
+{
+    [TestClass]
+    public class NaturalNumberTests
+    {
+        // This is a sanity check test.
+        // Ensure everthing is hooked up correctly AND
+        // that the permissions are correct.
+        [TestMethod]
+        public void EnsureICanCreateInstance()
+        {
+            NaturalNumber naturalNumber = new NaturalNumber();
+            Assert.IsNotNull(naturalNumber);
+        }
+
+        [TestMethod]
+        public void EnsureOneIsTheFirstNumber()
+        {
+            // Arrange (Where you set stuff up)
+            NaturalNumber naturalNumber = new NaturalNumber();
+
+            // Act (Call the method you're testing)
+            int expectedResult = 1;
+            int actualResult = naturalNumber.GetFirst();
+
+            // Assert (Check the output from your method)
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void EnsureNextIsTheNextNumber()
+        {
+            // Arrange (Where you set stuff up)
+            NaturalNumber naturalNumber = new NaturalNumber();
+
+            // Act (Call the method you're testing)
+            int expectedResult = 3 + 1;
+            int actualResult = naturalNumber.GetNext(3);
+
+            // Assert (Check the output from your method)
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void EnsureNumsAreRequestedNums()
+        {
+            // Arrange (Where you set stuff up)
+            NaturalNumber naturalNumber = new NaturalNumber();
+
+            // Act (Call the method you're testing)
+            int[] expectedResult = new int[] { 1, 2, 3 };
+            int[] actualResult = naturalNumber.GetSequence(3);
+
+            // Assert (Check the output from your method)
+            CollectionAssert.Equals(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void EnsurePrintNumbersWork()
+        {
+            // Arrange (Where you set stuff up)
+            NaturalNumber naturalNumber = new NaturalNumber();
+
+            // Act (Call the method you're testing)
+            string expectedResult = "1 2 3";
+            string actualResult = naturalNumber.PrintNumbers(naturalNumber.GetSequence(3));
+
+            // Assert (Check the output from your method)
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
