e0734c6c975a06e31ceea20ccfbfd5ad5e3ca2a7
Jurnell Cockhren <jurnell@sophicware.com>
2017-01-28 10:21:42 -0800

    Started example of implementing EvenNUmber with an interface explicitly.

---
+++ MathMagician/Numbers/EvenNumberWithInterface.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMagician.Numbers
+{
+    public class EvenNumberWithInterface : Integer
+    {
+        private int First { get; set; }
+
+        public EvenNumberWithInterface()
+        {
+            First = 2;
+        }
+
+        public int GetFirst()
+        {
+            return First;
+        }
+
+        public int GetNext(int current)
+        {
+            return current + 2;
+        }
+
+        public int[] GetSequence(int how_many)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string PrintNumbers(int[] how_many)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MathMagicianTests/Numbers/EvenNumberTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathMagician.Numbers;
+
+namespace MathMagicianTests.Numbers
+{
+    [TestClass]
+    public class EvenNumberTests
+    {
+        [TestMethod]
+        public void EnsureICanCrestInstance()
+        {
+            EvenNumberWithInterface evenNumber = new EvenNumberWithInterface();
+
+            Assert.IsNotNull(evenNumber);
+        }
+
+        [TestMethod]
+        public void EnsureOneIsTheFirstNumber()
+        {
+            // Arrange (Where you set stuff up)
+            EvenNumberWithInterface evenNumber = new EvenNumberWithInterface();
+
+            // Act (Call the method you're testing)
+            int expectedResult = 2;
+            int actualResult = evenNumber.GetFirst();
+
+            // Assert (Check the output from your method)
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        [TestMethod]
+        public void EnsureICanGetNext()
+        {
+            // Arrange
+            EvenNumberWithInterface evenNumber = new EvenNumberWithInterface();
+
+            // Act
+            int expectedResult = 8;
+            //int myNum; this is zero "0"
+            int actualResult = evenNumber.GetNext(6);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        /*
+        [TestMethod]
+        public void EnsureICanGetSequence()
+        {
+            // Arrange
+            NaturalNumber naturalNumber = new NaturalNumber();
+
+            // Act
+            int[] expectedResult = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //int myNum; this is zero "0"
+            int[] actualResult = naturalNumber.GetSequence(10);
+
+            // Assert
+            Assert.AreEqual(expectedResult.Length, actualResult.Length);
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void EnsureICanPrintSequence()
+        {
+            // Arrange
+            NaturalNumber naturalNumber = new NaturalNumber();
+
+            // Act
+            string expectedResult = "1 2 3 4 5";
+            int[] inputArray = new[] { 1, 2, 3, 4, 5 };
+            string actualResult = naturalNumber.PrintNumbers(inputArray);
+            //string actualResult = naturalNumber.PrintNumbers(new[] { 1, 2, 3, 4, 5 });
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        */
+    }
+}
