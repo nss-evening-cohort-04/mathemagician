d3d95b1bb25fa425430775d6570e0b18ba426de0
Alex Saber <Alex Saber>
2017-01-28 12:46:01 -0600

    add EvenNumber and EvenNumberTest files

---
+++ MathMagician/Numbers/EvenNumber.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMagician.Numbers
+{
+    public class EvenNumber : Integer
+    {
+        private int First { get; set; }
+
+        public EvenNumber()
+        {
+            First = 2;
+        }
+
+        public int GetFirst()
+        {
+            return First;
+        }
+
+        public int GetNext(int current)
+        {
+            return current + 2;
+        }
+
+        public int[] GetSequence(int how_many)
+        {
+            int[] numbers = new int[how_many];
+            numbers[0] = GetFirst();
+            for (var i = 1; i < numbers.Length; i++)
+            {
+                numbers[i] = GetNext(numbers[i-1]);
+            }
+            return numbers;
+        }
+
+        public string PrintNumbers(int[] number_array)
+        {
+            return String.Join(" ", number_array);
+        }
+    }
+}
---
+++ MathMagicianTests/Numbers/EvenNumberTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathMagician.Numbers;
+
+
+namespace MathMagicianTests.Numbers
+{
+    [TestClass]
+    public class EvenNumberTests
+    {
+        [TestMethod]
+        public void EnsureICanCreateInstance()
+        {
+            EvenNumber evenNumber = new EvenNumber();
+
+            Assert.IsNotNull(evenNumber);
+        }
+
+        [TestMethod]
+        public void EnsureICanGetFirstEvenNumber()
+        {
+            // Arrange (Where you set stuff up)
+            EvenNumber evenNumber = new EvenNumber();
+
+            // Act (Call the method you're testing)
+            int expectedResult = 2;
+            int actualResult = evenNumber.GetFirst();
+
+            // Assert (Check the output from your method)
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void EnsureICanGetNextEvenNumber()
+        {
+            // Arrange (Where you set stuff up)
+            EvenNumber evenNumber = new EvenNumber();
+
+            // Act (Call the method you're testing)
+            int expectedResult = 4;
+            int actualResult = evenNumber.GetNext(2);
+
+            // Assert (Check the output from your method)
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void EnsureICanGetSequenceOfEvenNumbers()
+        {
+            // Arrange (Where you set stuff up)
+            EvenNumber evenNumber = new EvenNumber();
+
+            // Act (Call the method you're testing)
+            int[] expectedResult = { 2, 4, 6, 8, 10 };
+            int[] actualResult = evenNumber.GetSequence(5);
+
+            // Assert (Check the output from your method)
+            Assert.AreEqual(expectedResult.Length, actualResult.Length);
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void EnsureICanPrintSequenceOfEvenNumbers()
+        {
+            // Arrange (Where you set stuff up)
+            EvenNumber evenNumber = new EvenNumber();
+
+            // Act (Call the method you're testing)
+            string expectedResult = "2 4 6 8 10";
+            string actualResult = evenNumber.PrintNumbers(new[] { 2, 4, 6, 8, 10 });
+
+            // Assert (Check the output from your method)
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
