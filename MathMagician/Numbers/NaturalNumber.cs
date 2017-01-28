736c9dd6272a7aea8aaa798764f998ec1af6286b
Alicia <akb.miceli@gmail.com>
2017-01-28 12:01:09 -0600

    added even number and even number test

--- MathMagician/Numbers/NaturalNumber.cs
+++ MathMagician/Numbers/NaturalNumber.cs
@@ -31,12 +31,20 @@ namespace MathMagician.Numbers
 
         public int[] GetSequence(int how_many)
         {
-            throw new NotImplementedException();
+            int[] numbers = new int[how_many];
+            numbers[0] = GetFirst(); // i = 1
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                numbers[i] = GetNext(numbers[i - 1]);
+            }
+
+            return numbers;
+
         }
 
-        public string printNumbers(int[] how_many)
+        public string printNumbers(int[] number_array)
         {
-            throw new NotImplementedException();
+            return String.Join(" ", number_array);
         }
     }
 }
