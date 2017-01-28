173ffe2b5b6a9e7c1f0f0ebed9e5b620f2379e6d
sward42 <sarahward42@gmail.com>
2017-01-27 23:52:22 -0600

    add program functionality

--- MathMagician/Program.cs
+++ MathMagician/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MathMagician.Numbers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,14 @@ namespace MathMagician
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What would you like for me to do?");
-            Console.WriteLine(""); // "How" you provide the user useful info is on you.
+            Console.WriteLine("What type of numbers would you like me to print?");
+            Console.WriteLine("Integer");
+            Console.WriteLine("Even");
+            Console.WriteLine("Odd");
+            Console.WriteLine("Prime");
+            Console.WriteLine("Fibonacci");
+
+            // "How" you provide the user useful info is on you.
             // Think about, "How will the user pick a command?"
             // Is it better to modify the original question?  Make it more useful or relevant?
 
@@ -26,10 +33,39 @@ namespace MathMagician
 
             // When do I check if the "How Many" response is too big?
             //If they enter a number > 30, how do I respond AND exit the program?
-
-            Console.WriteLine($"Cool, I'm going to print {how_many} {command} numbers.");
+            int howMany = Int32.Parse(how_many);
+            string commandLower = command.ToLower();
 
             // Okay!!! Print some stuff!!!
+
+            if (howMany <= 30)
+            {
+                
+
+                if (commandLower == "integer")
+                {
+                    Console.WriteLine($"Cool, I'm going to print {how_many} {command} numbers.");
+                    NaturalNumber naturalNumber = new NaturalNumber();
+                    Console.WriteLine(naturalNumber.printNumbers(naturalNumber.GetSequence(howMany)));
+                }
+                else if (commandLower == "even")
+                {
+                    Console.WriteLine($"Cool, I'm going to print {how_many} {command} numbers.");
+                    EvenNumber evenNumber = new EvenNumber();
+                    Console.WriteLine(evenNumber.printNumbers(evenNumber.GetSequence(howMany)));
+                }
+                else
+                {
+                    Console.WriteLine("Please select a number type from the list provided.");
+                }
+            } else
+            {
+                Console.WriteLine("Please input a number less than or equal to 30.");
+            }
+            
+          
+
+           
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
