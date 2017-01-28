2fdf34df5131434d137204e932c821dbc2771c6b
Jurnell Cockhren <jurnell@sophicware.com>
2017-01-28 12:06:20 -0800

    EvenNumber with implementing interface explicitly

--- MathMagician/Numbers/EvenNumberWithInterface.cs
+++ MathMagician/Numbers/EvenNumberWithInterface.cs
@@ -27,12 +27,19 @@ namespace MathMagician.Numbers
 
         public int[] GetSequence(int how_many)
         {
-            throw new NotImplementedException();
+            int[] numbers = new int[how_many];
+            numbers[0] = GetFirst(); // i = 1
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                numbers[i] = GetNext(numbers[i - 1]);
+            }
+
+            return numbers;
         }
 
-        public string PrintNumbers(int[] how_many)
+        public string PrintNumbers(int[] number_array)
         {
-            throw new NotImplementedException();
+            return String.Join(" ", number_array);
         }
     }
 }
