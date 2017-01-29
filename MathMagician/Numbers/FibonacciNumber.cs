89f58433ef0c6fdf93e2ec081acdbf440f5a69ff
LibE4 <libin_75@hotmail.com>
2017-01-29 08:56:55 -0800

    fix issue in fibonacci

--- MathMagician/Numbers/FibonacciNumber.cs
+++ MathMagician/Numbers/FibonacciNumber.cs
@@ -16,12 +16,15 @@ namespace MathMagician.Numbers
         public override int[] GetSequence(int how_many)
         {
             int[] fibonacciNums = new int[how_many];
-            fibonacciNums[0] = GetFirst();
-            fibonacciNums[1] = 1;
+            fibonacciNums[0] = GetFirst(); // set first
+            if (how_many >=2)
+            {
+                fibonacciNums[1] = 1;
+            } // set second
             for (int i = 2; i < how_many; i++)
             {
                 fibonacciNums[i] = fibonacciNums[i-1] + fibonacciNums[i-2];
-            }
+            } // set those after second
             return fibonacciNums;
         }
     }
