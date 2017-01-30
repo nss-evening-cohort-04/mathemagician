ad23881060cafe9eeb950eda61ef0b1deec2c010
Alex Saber <Alex Saber>
2017-01-30 17:12:16 -0600

    refactor Fibonacci to inherit from Natural

---
+++ MathMagicianTests/Numbers/FibonacciNumberTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathMagician.Numbers;
+
+namespace MathMagicianTests.Numbers
+{
+    [TestClass]
+    public class FibonacciNumberTests
+    {
+        [TestMethod]
+        public void EnsureICanCreateFibInstance()
+        {
+            FibonacciNumber fibonacciNumber = new FibonacciNumber();
+            Assert.IsNotNull(fibonacciNumber);
+        }
+    }
+}
