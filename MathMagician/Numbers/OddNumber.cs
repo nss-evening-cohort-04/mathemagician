353d255d1c799270bf5ca45bde5136b931952925
Justin Leggett <justin@codingrookie.com>
2017-01-30 16:44:51 -0600

    completed 2/3 of oddnumbers

---
+++ MathMagician/Numbers/OddNumber.cs
@@ -0,0 +1,46 @@
+﻿using MathMagician.Numbers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMagician.Numbers
+{
+    public class OddNumber : Integer
+    {
+        private int First { get; set; }
+
+        public OddNumber()
+        {
+            First = 1;
+        }
+        public int GetFirst()
+        {
+            return First;
+        }
+
+        public int GetNext(int current)
+        {
+            if((current % 2) == 0)
+            {
+                current = current + 1;
+            }
+            else
+            {
+                current = current + 2;
+            }
+            return current;
+        }
+
+        public int[] GetSequence(int howMany)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string PrintNumbers(int[] howMany)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MathMagicianTests/Numbers/OddNumberTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathMagician.Numbers;
+
+namespace MathMagicianTests.Numbers
+{
+    [TestClass]
+    public class OddNumberTests
+    {
+        [TestMethod]
+        public void EnsureICanCreateInstance()
+        {
+            OddNumber oddNumber = new OddNumber();
+
+            Assert.IsNotNull(oddNumber);
+        }
+
+        [TestMethod]
+        public void EnsureFirstNumberIsOdd()
+        {
+            // Arrange
+            OddNumber oddNumber = new OddNumber();
+
+            // Act 
+            int expectedResult = 1;
+            int actualResult = oddNumber.GetFirst();
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void EnsureNumberPassedPlusOne()
+        {
+            // Arrange
+            OddNumber oddNumber = new OddNumber();
+
+            // Act
+            int expectedResult = 3;
+            int actualResult = oddNumber.GetNext(1);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
