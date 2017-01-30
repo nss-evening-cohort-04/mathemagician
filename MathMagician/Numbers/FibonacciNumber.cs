32d2e6651b24df32be8056fe1ed8d40ad4bc4dac
Alex Saber <Alex Saber>
2017-01-30 17:40:33 -0600

    implement Fibonacci class and FibonacciNumberTests

--- MathMagician/Numbers/FibonacciNumber.cs
+++ MathMagician/Numbers/FibonacciNumber.cs
@@ -8,6 +8,29 @@ namespace MathMagician.Numbers
 {
     public class FibonacciNumber : NaturalNumber
     {
+        private int Current { get; set; }
 
+        public FibonacciNumber()
+        {
+            First = 1;
+            Current = 1;
+        }
+
+        public int GetCurrent()
+        {
+            return Current;
+        }
+
+        public override int[] GetSequence(int how_many)
+        {
+            int[] fibArray = new int[how_many];
+            fibArray[0] = GetFirst();
+            for (var i = 1; i < how_many; i++)
+            {
+                fibArray[i] = GetCurrent();
+                Current = fibArray[i] + fibArray[i - 1];
+            }
+            return fibArray;
+        }
     }
 }
--- MathMagicianTests/Numbers/FibonacciNumberTests.cs
+++ MathMagicianTests/Numbers/FibonacciNumberTests.cs
@@ -13,5 +13,61 @@ namespace MathMagicianTests.Numbers
             FibonacciNumber fibonacciNumber = new FibonacciNumber();
             Assert.IsNotNull(fibonacciNumber);
         }
+
+        [TestMethod]
+        public void EnsureICanGetFirstFibNumber()
+        {
+            FibonacciNumber fibonacciNumber = new FibonacciNumber();
+
+            int expectedResult = 1;
+            int actualResult = fibonacciNumber.GetFirst();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void EnsureICanGetNext()
+        {
+            FibonacciNumber fibonacciNumber = new FibonacciNumber();
+
+            int expectedResult = 4;
+            int actualResult = fibonacciNumber.GetNext(3);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void EnsureICanGetCurrent()
+        {
+            FibonacciNumber fibonacciNumber = new FibonacciNumber();
+
+            int expectedResult = 1;
+            int actualResult = fibonacciNumber.GetCurrent();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void EnsureICanGetFibSequence()
+        {
+            FibonacciNumber fibonacciNumber = new FibonacciNumber();
+
+            int[] expectedResult = { 1, 1, 2, 3, 5, 8, 13 };
+            int[] actualResult = fibonacciNumber.GetSequence(7);
+
+            Assert.AreEqual(expectedResult.Length, actualResult.Length);
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void EnsureICanPrintNubers()
+        {
+            FibonacciNumber fibonacciNumber = new FibonacciNumber();
+
+            string expectedResult = "1 1 2 3 5 8 13";
+            string actualResult = fibonacciNumber.PrintNumbers(new int[] { 1, 1, 2, 3, 5, 8, 13 });
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
     }
 }
