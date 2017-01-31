6efc06884555625baa7c05578b1ecbde8fea8f6a
blairvansant <blairbot@gmail.com>
2017-01-31 16:51:05 -0600

    ensured I can get the next odd number

--- MathMagicianTests/Numbers/OddTest.cs
+++ MathMagicianTests/Numbers/OddTest.cs
@@ -16,6 +16,22 @@ namespace MathMagicianTests.Numbers
             Assert.IsNotNull(oddNumber);
         }
 
+        [TestMethod]
+        public void EnsureICanGetTheNextNumberOdd()
+        {
+            // Arrange 
+            OddNumber oddNumber = new OddNumber();
+
+            // Act 
+            int expectedResult = 5;
+            int actualResult = oddNumber.GetNext(3);
+
+            // Assert 
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+      
+
+        }
+
        
     }
-}
